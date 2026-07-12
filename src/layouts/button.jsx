6227d2ee8a5b392f0bc959a3b2a76fcc126b1465
Suhaib bin tariq <suhaibbintariq1@gmail.com>
2026-07-12 16:11:04 +0500

    initial commit

--- src/layouts/button.jsx
+++ src/layouts/button.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const Button = ({ title, backgroundcolor }) => {
-    return (
-        <button
-            className={`rounded-full px-10 py-2 font-medium border-4 border-[#00b934] text-black transition-all duration-300 hover:bg-[#00b934] hover:text-white active:bg-[#00b934] active:text-white hover:border-white active:border-white ${backgroundcolor || "bg-white"}`}
-        >
-            {title}
-        </button>
-    );
+const Button = (props) => {
+  return (
+    <div>
+      <button
+        className={` ${props.backgroundColor} text-black rounded-full px-8 py-2 font-medium hover:bg-[#8482FF] hover:text-white transition-all`}
+      >
+        {props.title}
+      </button>
+    </div>
+  );
 };
 
 export default Button;
